25c9da17b19d1c84bfb88d898b1e87b047abeb7b
Ahmed Faraz <ahmedjamshed79@gmail.com>
2022-07-17 21:01:46 +0500

    nav bar complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { PublicRecordComponent } from './public-record/public-record.component';
+import { PoliceRegisterComponent } from './police-register/police-register.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PoliceRecordComponent } from './police-record/police-record.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: NavBarComponent
+  },
+  {
+    path:"police-register",
+    component: PoliceRegisterComponent,
+  },
+  {
+    path:"police-record",
+    component: PoliceRecordComponent,
+  },
+  {
+    path:"public-record",
+    component: PublicRecordComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
